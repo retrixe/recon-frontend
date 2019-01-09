4a905b5baaa97c5311d19eda78ad988e502c8da1
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2019-01-09 18:00:32 +0530

    Update and add even more packages.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,26 @@
 import * as React from 'react'
+import {
+  AppBar, Toolbar, Button, Typography
+} from '@material-ui/core'
+import Link from 'next/link'
+import withRoot from '../components/withRoot'
 
-export default (
-  <p>sup</p>
+const Index = () => (
+  <div style={{ marginRight: 16, marginLeft: 16 }}>
+    <head>
+      <title>ReCon-Frontend</title>
+      {/* <meta property='og:url' content={`${rootURL}/`} /> */}
+      {/* <meta property='og:description' content='' /> */}
+      {/* <meta name='Description' content='IveBot is a multi-purpose Discord bot.' /> */}
+    </head>
+    <AppBar>
+      <Toolbar>
+        <Typography variant='title' color='inherit' style={{ flex: 1 }}>ReCon</Typography>
+        <Link prefetch href='/about'><Button color='inherit'>About</Button></Link>
+      </Toolbar>
+    </AppBar>
+    <br /><br /><br /><br />
+  </div>
 )
+
+export default withRoot(Index)
