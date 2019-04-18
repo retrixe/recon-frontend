348d79da06630d9f8132ade227052d4bb2bf9dcf
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2019-04-18 23:40:50 +0530

    Most support for the WebSocket gateway for console implemented.

--- components/dashboard/console.tsx
+++ components/dashboard/console.tsx
@@ -7,76 +7,54 @@ import Check from '@material-ui/icons/Check'
 import { ip } from '../../config.json'
 import * as fetch from 'isomorphic-unfetch'
 
-interface OperatorsList {
-  code: number, operators: ({ name: string, uuid: string })[]
-}
-
 interface S { // eslint-disable-next-line no-undef
-  operators?: OperatorsList, listening: boolean, interval?: NodeJS.Timeout, username: string
+  console?: string, listening: boolean, ws?: WebSocket, command: string
 }
 
 export default class Console extends React.Component<{}, S> {
   constructor (props: {}) {
     super(props)
-    this.state = { listening: false, username: '' }
-    this.opPlayer = this.opPlayer.bind(this)
+    this.state = { listening: false, command: '' }
+    this.executeCommand = this.executeCommand.bind(this)
   }
 
   async componentDidMount () {
     try {
-      // Fetch operators.
-      try {
-        const operators = await (await fetch(ip + ':4200/operators', {
-          headers: { 'Access-Token': localStorage.getItem('accessToken') }
-        })).json()
-        if (operators.code === 401) throw new Error()
-        this.setState({ operators, listening: true })
-      } catch (e) { }
-      // Set an interval of 30 seconds to repeatedly update the operator list.
-      const interval = setInterval(async () => {
-        // Try to access the server.
-        try {
-          // Update the status.
-          const operators = await (await fetch(ip + ':4200/operators', {
-            headers: { 'Access-Token': localStorage.getItem('accessToken') }
-          })).json()
-          // If unauthorized, throw an error right now.
-          // This will be handled later.
-          if (operators.code === 401) throw new Error()
-          this.setState({ operators, listening: true })
-        } catch (e) {
-          // We set listening to false.
-          this.setState({ listening: false })
+      // Connect to console.
+      let ws = new WebSocket(`${ip}:4269`)
+      this.setState({ ws, listening: true })
+      // Register listeners.
+      ws.onerror = () => {
+        console.error('Looks like an error occurred while connected to the gateway.')
+      }
+      ws.onmessage = (event) => this.setState({ console: event.data })
+      ws.onclose = (event) => {
+        if (!event.wasClean) {
+          // Something.. later.
         }
-        // Should happen every 30 seconds.
-      }, 30000)
-      // This can be server-side rendering error so we won't log on catch.
-      // Client side rendering will alert the user anyways.
-      // We set the interval in state to unregister it later..
-      this.setState({ interval })
-    } catch (e) { }
+      }
+    } catch (e) {}
   }
 
-  // Clear interval on timeout.
-  componentWillUnmount () { clearInterval(this.state.interval) }
+  // Close WebSocket when done.
+  componentWillUnmount () { this.state.ws.close() }
 
-  async opPlayer () {
+  async executeCommand () {
     try {
       const request = await (await fetch(
-        `${ip}:4200/operators/addPlayerByName?name=${this.state.username}`,
-        { headers: { 'Access-Token': localStorage.getItem('accessToken') } }
+        `${ip}:4200/console/execute`, {
+          headers: { 'Access-Token': localStorage.getItem('accessToken') },
+          body: this.state.command
+        }
       )).json()
-      if (!request.success) console.warn('Unable to op player.')
-      else {
-        this.setState({ username: '' })
-        await this.componentDidMount()
-      }
-    } catch (e) { }
+      if (!request.success) console.warn('Unable to execute command.')
+      else this.setState({ command: '' })
+    } catch (e) {}
   }
 
   render () {
     // Return the code.
-    if (!this.state.listening || !this.state.operators) {
+    if (!this.state.listening || !this.state.command) {
       return (
         <Paper style={{ padding: 10 }}>
           <Typography>Looks like we can{`'`}t connect to the server. Oops!</Typography>
@@ -85,10 +63,6 @@ export default class Console extends React.Component<{}, S> {
           </Typography>
         </Paper>
       )
-    }
-    const um = []
-    for (let i = 0; i < 400; i++) {
-      um.push(<Typography key={i}>um</Typography>)
     }
     return (
       <>
@@ -102,17 +76,19 @@ export default class Console extends React.Component<{}, S> {
             backgroundColor: '#111111',
             height: '60vh'
           }}>
-            <div style={{ height: '100%', width: '100%', overflow: 'auto' }}>{um}</div>
+            <div style={{ height: '100%', width: '100%', overflow: 'auto' }}>
+              {this.state.console}
+            </div>
           </Paper>
           <Divider />
           <Paper elevation={10} style={{ padding: 10, display: 'flex' }}>
             <TextField
-              label='Input' value={this.state.username} fullWidth
-              onChange={e => this.setState({ username: e.target.value })}
-              onSubmit={this.opPlayer} color='secondary'
-              onKeyPress={e => e.key === 'Enter' && this.opPlayer()}
+              label='Input' value={this.state.command} fullWidth
+              onChange={e => this.setState({ command: e.target.value })}
+              onSubmit={this.executeCommand} color='secondary'
+              onKeyPress={e => e.key === 'Enter' && this.executeCommand()}
             /><div style={{ width: 10 }} />
-            <Fab color='secondary' onClick={this.opPlayer}><Check /></Fab>
+            <Fab color='secondary' onClick={this.executeCommand}><Check /></Fab>
           </Paper>
         </Paper>
       </>
